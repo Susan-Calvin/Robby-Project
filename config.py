97ac79c5dd98199c485e4e970c95b897e2530a44
Susan-Clavin <ludmila.vishnyak@gmail.com>
2022-08-21 16:21:33 +0300

    Edited procfile

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5196192972:AAGYH6OP7KXiaDd4bvZZXB5PTw3iAJY7DvQ"
 APP_URL = "https://robbythetelegrambot.herokuapp.com/" + BOT_TOKEN
+DATABASE_URI = "postgres://esiusrztrfstzk:3950c569251018c50dad9ffbc5575910c45849216491889523789c4a44143f75@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/d3bkol7am2pre9"
